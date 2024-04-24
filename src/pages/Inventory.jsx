8dd776e7f928b46041fd2b87f5b948218a2724de
Papiorules <152616107+Papiorules@users.noreply.github.com>
2024-04-24 19:15:45 +0100

    Update Inventory.jsx

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,14 +1,33 @@
 
-
-const Inventory = ()=>{
-
-    return(
-
-        <>
+const Inventory = () => {
+    const [inventory, setInventory] = useState([]);
+  
+    const handleAddItem = (newItem) => {
+      setInventory([...inventory, { ...newItem, id: Date.now() }]);
+    };
+  
+    const handleRemoveItem = (id) => {
+      setInventory(inventory.filter(item => item.id !== id));
+    };
+  
+    const handleUpdateItem = (updatedItem) => {
+      setInventory(inventory.map(item => (item.id === updatedItem.id ? updatedItem : item)));
+    };
+  
+    return (
+      <div>
+        <h2>Inventory</h2>
+        <AddInventoryItem onAdd={handleAddItem} />
         <div>
-        Inventory
+          {inventory.map(item => (
+            <div key={item.id}>
+              <InventoryItem item={item} onRemove={handleRemoveItem} />
+              <UpdateInventoryItem item={item} onUpdate={handleUpdateItem} />
+            </div>
+          ))}
         </div>
-        </>
-    )
-}
-export default Inventory
+      </div>
+    );
+  }
+  
+  export default Inventory;
